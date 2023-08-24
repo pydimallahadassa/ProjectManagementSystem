9fc3d36b4e3ffb5adc1ea047c13041531ad607fb
Pydimalla Hadassa <gracepydimalla@gmail.com>
2023-08-24 11:25:30 +0530

    complete files

--- jwt-authentication/src/main/java/com/authentication/models/ERole.java
+++
@@ -1,8 +0,0 @@
-package com.authentication.models;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-
-}
--- login/src/main/java/com/example/demo/exception/PasswordNotSameException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-@SuppressWarnings("serial")
-public class PasswordNotSameException extends Exception{
-	public PasswordNotSameException(String msg) {
-		super(msg);
-	}
-
-}
